8be541fec805a3ff1c24f67c7f1f6a5a4342692c
EvhYa <nicetea.bro@gmail.com>
2023-06-23 21:32:39 +0300

    hw_V1.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGalleryMarkup = galleryItems
+   .map(
+      ({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+      </a></li>`
+   )
+   .join("");
+
+gallery.insertAdjacentHTML("afterbegin", createGalleryMarkup);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+   evt.preventDefault();
+   if (evt.target.classList.contains("gallery__image")) {
+      const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+      instance.show();
+      window.addEventListener("keyup", onKeyup);
+      function onKeyup(evt) {
+         if (evt.code === "Escape" && instance.visible()) {
+            console.log(evt.code);
+            instance.close();
+         }
+      }
+   }
+}
+
+// event.key === "Escape";
+// console.log(galleryItems);
+// console.log(createGalleryMarkup);
